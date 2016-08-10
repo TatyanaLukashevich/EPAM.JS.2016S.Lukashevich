9636ff69178fa987aacb07f0494eb50c34484e65
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-08-10 18:33:32 +0300

    done

--- day1/scripts/log.js
+++ day1/scripts/log.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() { 
 for(var i=0; i<data.length;i++){
 	var element;
 	if(typeof(data[i]) == "undefined" ){
@@ -13,4 +14,5 @@ for(var i=0; i<data.length;i++){
 	}
 	
 	  console.log("data[{0}]={1}", i, element);
+}
 }
