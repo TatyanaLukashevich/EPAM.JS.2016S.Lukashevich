f79f5a7c006c661ea95aed7941a9315970ecd642
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-08-10 17:56:10 +0300

    main task done

--- day1/scripts/calc.js
+++ day1/scripts/calc.js
@@ -1,17 +1,18 @@
-for(var i=0; i<data.length;i++){
-	if(typeof(data[i]) != "number"  && !parseInt(data[i]) ){
+for(var i = 0; i < data.length; i++){
+	var element=Number(data[i])
+	if(data[i]!=0 && !element ){
 		continue;
 	}
 	
 	else if(data[i] == 0){
-		data[i]+=10;
+		data[i] = element + 10;
 	}
 	
 	else if(data[i]>100){
-		data[i]-=100;
+		data[i] = element - 100;
 	}
 	
 	else if(data[i]<100){
-		data[i]+=100;
+		data[i] = element + 100;
 	}
 }
