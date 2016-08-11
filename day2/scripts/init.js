679c917291b3157dd12a5480017d9f49d20950af
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-08-11 21:23:58 +0300

    done main part

--- day2/scripts/init.js
+++ day2/scripts/init.js
@@ -3,7 +3,7 @@ var data = [];
 for(var i = 0; i < 5; i++){
 	var type = random(1, 3);
 	data[i] = {};
-	data[i]["count"] = random(1, 10);
+	data[i].count = random(1, 10);
 	data[i]["getCount" + type] = function(){ 
 		return this.count;
 	};
