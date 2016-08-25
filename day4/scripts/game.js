5a71fb752addaa585649a806a84808419e95f9ca
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-08-25 23:29:53 +0300

    added pause/unpause

--- day4/scripts/game.js
+++ day4/scripts/game.js
@@ -5,49 +5,101 @@ $(function() {
 	var zombieTimeout = 100;
 	var $field = $('#field');
 	
-	$("#gnrt").on("click", generate);
+	var isPaused = false;
+	var click = 0;
+	
+	$("#start").on("click", generate);
+	$("#pause").on("click", pause);
 	$("#slowUp").on("click", slowUpZombie);
 	$("#growOld").on("click", growOldZombie);
 	$("#expld").on("click", explode);
 	
 	function generate() {
-		$(".game-over").css("display","none");
-		var randomZombie = random(0, 1);
-		var randomLine = random(1, randomLineCount);
-		var $fieldLine = $("#" + randomLine);
-		var currentZombie = new zombies[randomZombie]($fieldLine, randomZombie);
-		zombiesArray.push(currentZombie);
-		var intervalId = setInterval(function(){
-			if(!currentZombie.move()){
-				clearTimeout(intervalId);
-				gameOver();
+			var gameId = setInterval(function() {
+			$(".game-over").css("display","none");
+			var randomZombie = random(0, 1);
+			var randomLine = random(1, randomLineCount);
+			var $fieldLine = $("#" + randomLine);
+			var currentZombie = new zombies[randomZombie]($fieldLine, randomZombie);
+			zombiesArray.push(currentZombie);
+			if(isPaused) {
+				clearTimeout(gameId);
 			}
-		},100);
-	}
+			var intervalId = setInterval(function(){
+				if(!currentZombie.move()){
+					clearTimeout(intervalId);
+					gameOver();
+				}
+				else if(isPaused){
+					clearTimeout(intervalId);
+				}
+			},100);
+				}, 2500)
+		$('#start').css("background", "gray");	
+
+	};
+				
+	
 
 	
 	function slowUpZombie() {
-		$(zombiesArray).each(function(index, element) {
+		if(!isPaused) {
+			$('#slowUp').css("border", "1px solid red");	
+			$(zombiesArray).each(function(index, element) {
 			zombiesArray[index].slowUp();
-		})
+			})
+			setInterval(function(){$('#slowUp').css("border", "none")}, 200);
+		}
 	}
 	
 	function growOldZombie() {
-		$(zombiesArray).each(function(index, element) {
-			zombiesArray[index].growOld();
-		
-		})
+		if(!isPaused) {
+			$('#growOld').css("border", "1px solid red");	
+			$(zombiesArray).each(function(index, element) {
+			zombiesArray[index].growOld();		
+			})
+			setInterval(function(){$('#growOld').css("border", "none")}, 200);
+		}
 	}
 	
 	function explode() {
-		$(zombiesArray).each(function(index, element) {
-			this.explode(15);
-			if (zombiesArray[index].currentHealth  <= 0) {
-				zombiesArray[index].die()
+		if(!isPaused) {
+			$('#expld').css("border", "1px solid red");	
+			$(zombiesArray).each(function(index, element) {
+				this.explode(15);
+				if (zombiesArray[index].currentHealth  <= 0) {
+					zombiesArray[index].die()
 			}
-		})
+			})
+			setInterval(function(){$('#expld').css("border", "none")}, 200);
+		}
 	}
 
+	function pause() {
+		click++;
+		if(click%2!=0) {
+			isPaused = true;
+		}
+		else {
+			isPaused = false;
+			$(zombiesArray).each(function(index, element) {
+				var intervalId = setInterval(function(){
+				if(!zombiesArray[index].move()){
+					clearTimeout(intervalId);
+					gameOver();
+				}
+				else if(isPaused){
+					clearTimeout(intervalId);
+				}
+			},100);
+		})
+			generate();
+		}
+		$('#pause').css("border", "1px solid red");	
+
+		setInterval(function(){$('#pause').css("border", "none")}, 200);
+	}
+	
 	function gameOver(){
 		$(".game-over").css("display","block");
 		$(".zombie").remove();
